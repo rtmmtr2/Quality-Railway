015dba7ec50f115b9410991fda3a111fd45379f0
rtmmtr2 <xy_xujunjie@outlook.com>
2025-05-03 16:47:12 +0800

    add high_horn and low_horn

---
+++ src/main/java/com/qualityrailway/qr/items/high_horn.java
@@ -0,0 +1,9 @@
+package com.qualityrailway.qr.items;
+
+import net.minecraft.world.item.Item;
+
+public class high_horn extends Item {
+    public high_horn(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/qualityrailway/qr/items/low_horn.java
@@ -0,0 +1,9 @@
+package com.qualityrailway.qr.items;
+
+import net.minecraft.world.item.Item;
+
+public class low_horn extends Item {
+    public low_horn(Properties properties) {
+        super(properties);
+    }
+}
