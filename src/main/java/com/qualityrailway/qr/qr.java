572c7ecaf441e06be44c28d24e0fc2a27666c33d
rtmmtr2 <137679547+rtmmtr2@users.noreply.github.com>
2025-09-13 21:40:17 +0800

    fixed some bugs.

--- src/main/java/com/qualityrailway/qr/qr.java
+++ src/main/java/com/qualityrailway/qr/qr.java
@@ -3,7 +3,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraft.resources.ResourceLocation;
 @Mod(qr.MODID)
 public class qr {
     public static final String MODID = "qr";
