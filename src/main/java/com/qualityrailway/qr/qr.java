b9bb8973ce096f3c478ba9202a4574a4e01f9043
rtmmtr2 <137679547+rtmmtr2@users.noreply.github.com>
2025-11-01 22:05:51 +0800

    1.5.1-release

--- src/main/java/com/qualityrailway/qr/qr.java
+++ src/main/java/com/qualityrailway/qr/qr.java
@@ -1,16 +1,20 @@
 package com.qualityrailway.qr;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 @Mod(qr.MODID)
 public class qr {
     public static final String MODID = "qr";
-
+    public static final Logger LOGGER = LogManager.getLogger();
     public qr() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         // 注册方块，物品，声音等
@@ -18,13 +22,21 @@ public class qr {
         ModItems.ITEMS.register(bus);
         ModSounds.SOUNDS.register(bus);
         MinecraftForge.EVENT_BUS.register(this);
-        // 注册客户端设置事件监听器
+        // 注册双端设置事件监听器
         bus.addListener(this::onClientSetup);
-
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
+        bus.addListener(this::onCommonSetup);
+        // 只在客户端环境下注册EventHandler
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            MinecraftForge.EVENT_BUS.register(new EventHandler());
+        });
+        LOGGER.info("Quality Railway initialized successfully!");
     }
-    public static final Logger LOGGER = LogManager.getLogger();
+
     private void onClientSetup(final FMLClientSetupEvent event) {
         KeyBindings.register(event);
+        LOGGER.info("Client setup complete");
+    }
+    private void onCommonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Common setup complete");
     }
 }
